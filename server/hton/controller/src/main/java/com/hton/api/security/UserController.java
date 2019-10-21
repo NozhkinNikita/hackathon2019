34fc68a0240e71ca261d9f8d7196485f7829375d
NikitaNozhkin <you_mazafaka@mail.ru>
2019-10-21 18:23:19 +0300

    requset for update user and location

--- server/hton/controller/src/main/java/com/hton/api/security/UserController.java
+++ server/hton/controller/src/main/java/com/hton/api/security/UserController.java
@@ -2,8 +2,11 @@ package com.hton.api.security;
 
 import com.hton.api.FilterUtils;
 import com.hton.api.WebMvcConfig;
+import com.hton.api.requests.UserUpdateRequest;
 import com.hton.api.responses.UserLocationsResponse;
 import com.hton.dao.CommonDao;
+import com.hton.dao.filters.ComplexCondition;
+import com.hton.dao.filters.Operation;
 import com.hton.dao.filters.SearchCondition;
 import com.hton.dao.filters.SimpleCondition;
 import com.hton.domain.Location;
@@ -74,8 +77,43 @@ public class UserController {
     }
 
     @PutMapping(value = "/")
-    public ResponseEntity<?> updateUser(@RequestBody User user) {
-        userDao.update(user);
+    public ResponseEntity<?> updateUser(@RequestBody UserUpdateRequest request) {
+        User origUser = userDao.getById(request.getUser().getId());
+        if (request == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (request.getUser() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (origUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        request.getUser().setPwd(origUser.getPwd());
+        userDao.update(request.getUser());
+        request.getLocaiotns().forEach(l -> {
+            ComplexCondition condition = new ComplexCondition.Builder()
+                    .setOperation(Operation.AND)
+                    .setConditions(
+                            new SimpleCondition.Builder()
+                                    .setSearchField("userId")
+                                    .setSearchCondition(SearchCondition.EQUALS)
+                                    .setSearchValue(request.getUser().getId())
+                                    .build(),
+                            new SimpleCondition.Builder()
+                                    .setSearchField("locationId")
+                                    .setSearchCondition(SearchCondition.EQUALS)
+                                    .setSearchValue(l.getId())
+                                    .build()
+                    )
+                    .setMaskFields(Arrays.asList("id"))
+                    .build();
+            userLocationDao.getByCondition(condition).forEach(ul -> userLocationDao.remove(ul.getId()));
+            UserLocation userLocation = new UserLocation();
+            userLocation.setUser(request.getUser());
+            userLocation.setLocation(l);
+            userLocationDao.save(userLocation);
+
+        });
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
