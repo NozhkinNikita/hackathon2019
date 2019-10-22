e94cc27860438508beb27c0df452be7450733916
Dmitry <dikma@mail.ru>
2019-10-22 23:13:20 +0300

    remove unused import

--- server/hton/controller/src/main/java/com/hton/api/requests/PointCreateRequest.java
+++ server/hton/controller/src/main/java/com/hton/api/requests/PointCreateRequest.java
@@ -1,12 +1,9 @@
 package com.hton.api.requests;
 
-import com.hton.domain.RouterData;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -16,11 +13,5 @@ public class PointCreateRequest {
 
     private String name;
 
-//    private LocalDateTime begin;
-
-//    private LocalDateTime end;
-
     private String scanId;
-
-//    private List<RouterData> routerDatas;
 }
