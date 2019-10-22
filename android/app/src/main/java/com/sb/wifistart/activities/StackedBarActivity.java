ab875fae41487d2b0c64e0e583e0acb71e187377
LebedevAS <JKDSGJSKDJG@gmail.com>
2019-10-22 19:59:33 +0300

    Generate graphics with wifi scan's data main_activity refactor WifiReceiver refactor

--- android/app/src/main/java/com/sb/wifistart/activities/StackedBarActivity.java
+++ android/app/src/main/java/com/sb/wifistart/activities/StackedBarActivity.java
@@ -1,24 +1,30 @@
 package com.sb.wifistart.activities;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.sb.wifistart.R;
+import com.sb.wifistart.receiver.WifiReceiver;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StackedBarActivity extends AppCompatActivity {
 
     private BarChart stackedChart;
 
+    // Using example
     private ArrayList<BarEntry> defValues() {
         ArrayList<BarEntry> dataVals = new ArrayList<>();
         dataVals.add(new BarEntry(0, new float[]{2, 4.5f, 3}));
@@ -32,13 +38,50 @@ public class StackedBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stacked_bar);
 
+        WifiReceiver wifiReceiver = new WifiReceiverImpl(getApplicationContext());
+        wifiReceiver.registerReceiver(getApplicationContext());
+        wifiReceiver.startScan();
+
         stackedChart = findViewById(R.id.stackedBarChart);
 
-        int[] colorClass = new int[]{Color.BLUE, Color.CYAN, Color.RED};
-        BarDataSet barDataSet = new BarDataSet(defValues(), "WiFi signals");
-        barDataSet.setColors(colorClass);
+        Button restartScan = findViewById(R.id.restartScan);
+        restartScan.setOnClickListener(view -> {
+            restartScan.setVisibility(View.INVISIBLE);
+            stackedChart.clear();
+            wifiReceiver.startScan();
+        });
+    }
 
-        BarData barData = new BarData(barDataSet);
-        stackedChart.setData(barData);
+    private class WifiReceiverImpl extends WifiReceiver {
+
+        public WifiReceiverImpl(Context context){
+            super(context);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean success = intent.getBooleanExtra(getWifiManager().EXTRA_RESULTS_UPDATED, false);
+            if(success) {
+                setScanResults(getWifiManager().getScanResults());
+            }
+
+            List<String> ssidList = new ArrayList<>();
+            List<BarEntry> dataVals = new ArrayList<>();
+            for(int i = 0; i < getScanResults().size(); i++) {
+                ssidList.add(getScanResults().get(i).SSID + "\n" + "(" + getScanResults().get(i).frequency + ")");
+                dataVals.add(new BarEntry(i, -getScanResults().get(i).level));
+            }
+
+            XAxis xAxis = stackedChart.getXAxis();
+            xAxis.setLabelRotationAngle(45);
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(ssidList));
+
+            BarDataSet barDataSet = new BarDataSet(dataVals, "WiFi signals");
+            BarData barData = new BarData(barDataSet);
+            stackedChart.setData(barData);
+            stackedChart.invalidate();
+
+            findViewById(R.id.restartScan).setVisibility(View.VISIBLE);
+        }
     }
 }
--- android/app/src/main/java/com/sb/wifistart/receiver/WifiReceiver.java
+++ android/app/src/main/java/com/sb/wifistart/receiver/WifiReceiver.java
@@ -1,4 +1,4 @@
-package com.sb.wifistart.scaners;
+package com.sb.wifistart.receiver;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Parcelable;
 
+import com.sb.wifistart.activities.WifiInfoActivity;
 import com.sb.wifistart.dto.RouterData;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
@@ -16,36 +19,20 @@ import lombok.Data;
 import static android.content.Context.WIFI_SERVICE;
 
 @Data
-public class WifiScanner extends BroadcastReceiver {
+public abstract class WifiReceiver extends BroadcastReceiver {
 
     private WifiManager wifiManager;
     private IntentFilter intentFilter;
-    List<RouterData> routerDataList;
+    List<ScanResult> scanResults;
 
-    public WifiScanner(Context context) {
+    public WifiReceiver(Context context) {
         this.wifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
         intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
-        if(success) {
-            routerDataList.clear();
-            List<ScanResult> scanResults = wifiManager.getScanResults();
-            scanResults.forEach(scanResult -> {
-                RouterData routerData = new RouterData();
-                routerData.setBssid(scanResult.BSSID);
-                routerData.setSsid(scanResult.SSID);
-                routerData.setRssi(scanResult.level);
-                routerData.setChannel(scanResult.frequency);
-                routerData.setBssid(scanResult.BSSID);
-
-                routerDataList.add(routerData);
-            });
-        }
-    }
+    public abstract void onReceive(Context context, Intent intent);
 
     public void startScan() {
         wifiManager.startScan();
