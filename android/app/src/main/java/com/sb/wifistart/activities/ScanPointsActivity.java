d2c698d4c2491d7bb6e9554feeaa1b783815fe23
LebedevAS <JKDSGJSKDJG@gmail.com>
2019-10-24 23:45:06 +0300

    Point list was added

--- android/app/src/main/java/com/sb/wifistart/activities/ScanPointsActivity.java
+++ android/app/src/main/java/com/sb/wifistart/activities/ScanPointsActivity.java
@@ -16,6 +16,7 @@ import com.sb.wifistart.dto.Point;
 import com.sb.wifistart.dto.Scan;
 import com.sb.wifistart.httprequests.UserApi;
 import com.sb.wifistart.service.UserApiHolder;
+import com.sb.wifistart.utils.TableContentHelper;
 
 import java.util.List;
 
@@ -30,46 +31,91 @@ public class ScanPointsActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_scans);
+        setContentView(R.layout.activity_scan_points);
+        postData(getIntent().getStringExtra("scanId"));
+    }
 
-        Point point = null;
+    public void postData(String scanId) {
+        UserApi yourUserApi = UserApiHolder.getUserApi();
+        Call call = yourUserApi.getScan(scanId);
 
-        pointsListView = (TableLayout) findViewById(R.id.pointsList);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                /*This is the success callback. Though the response type is JSON, with Retrofit we get the response in the form of WResponse POJO class
+                 */
+                System.out.println("on response login");
+                if (response.body() != null) {
+                    Scan scan = (Scan) response.body();
 
-        if(point != null) {
-            TableRow row = new TableRow(getApplicationContext());
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            row.setLayoutParams(lp);
+                    List<Point> points = scan.getPoints();
 
-            TextView pointName = new TextView(getApplicationContext());
-            TextView pointBeginDate = new TextView(getApplicationContext());
-            TextView pointEndDate = new TextView(getApplicationContext());
-            TextView pointWasRescaned = new TextView(getApplicationContext());
+                    pointsListView = (TableLayout) findViewById(R.id.pointsList);
 
-            pointName.setText(point.getName());
-            pointName.setTextAppearance(android.R.style.TextAppearance_Large);
-            pointName.setGravity(Gravity.CENTER_HORIZONTAL);
+                    if(points.size() == 0) {
+                        pointsListView.addView(TableContentHelper.getRowWithEmptyMessage(getApplicationContext(), 4));
+                    } else {
+                        points.forEach(point -> {
+                            TableRow row = new TableRow(getApplicationContext());
+                            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+                            row.setLayoutParams(lp);
 
-            pointBeginDate.setText(point.getBegin().split("T")[0]);
-            pointBeginDate.setTextAppearance(android.R.style.TextAppearance_Large);
-            pointBeginDate.setGravity(Gravity.CENTER_HORIZONTAL);
+                            TextView pointName = new TextView(getApplicationContext());
+                            TextView pointBeginDate = new TextView(getApplicationContext());
+                            TextView pointEndDate = new TextView(getApplicationContext());
+                            TextView pointWasRescaned = new TextView(getApplicationContext());
 
-            pointEndDate.setText(point.getEnd().split("T")[0]);
-            pointEndDate.setTextAppearance(android.R.style.TextAppearance_Large);
-            pointEndDate.setGravity(Gravity.CENTER_HORIZONTAL);
+                            pointName.setText(point.getName());
+                            pointName.setTextAppearance(android.R.style.TextAppearance_Medium);
+                            pointName.setGravity(Gravity.CENTER_HORIZONTAL);
 
-            pointWasRescaned.setText(point.getIsRepeat()? "Да": "Нет");
-            pointWasRescaned.setTextAppearance(android.R.style.TextAppearance_Large);
-            pointWasRescaned.setGravity(Gravity.CENTER_HORIZONTAL);
+                            pointBeginDate.setText(point.getBegin().split("T")[0]);
+                            pointBeginDate.setTextAppearance(android.R.style.TextAppearance_Medium);
+                            pointBeginDate.setGravity(Gravity.CENTER_HORIZONTAL);
 
-            row.addView(pointName);
-            row.addView(pointBeginDate);
-            row.addView(pointEndDate);
-            row.addView(pointWasRescaned);
+                            pointEndDate.setText(point.getEnd().split("T")[0]);
+                            pointEndDate.setTextAppearance(android.R.style.TextAppearance_Medium);
+                            pointEndDate.setGravity(Gravity.CENTER_HORIZONTAL);
 
-            row.setGravity(Gravity.CENTER_HORIZONTAL);
-            row.setPadding(5, 5, 5, 5);
-            pointsListView.addView(row);
-        }
+                            pointWasRescaned.setText(point.getIsRepeat()? "Да": "Нет");
+                            pointWasRescaned.setTextAppearance(android.R.style.TextAppearance_Medium);
+                            pointWasRescaned.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                            row.addView(pointName);
+                            row.addView(pointBeginDate);
+                            row.addView(pointEndDate);
+                            row.addView(pointWasRescaned);
+
+                            row.setGravity(Gravity.CENTER_HORIZONTAL);
+                            row.setPadding(5, 5, 5, 5);
+
+//                        row.setOnClickListener(view -> {
+//                            Intent intent = new Intent(MyScansActivity.this, ScanPointsActivity.class);
+//                            intent.putExtra("scanId", scan.getId());
+//                            startActivity(intent);
+//                        });
+
+                            pointsListView.addView(row);
+                        });
+                    }
+                } else {
+//                    info.setText("No of attempts remaining: " + counter);
+//                    // ToDo Do the same in NewScan activity
+//                    if (counter == 0) {
+//                        loginBtn.setEnabled(false);
+//                    }
+                    System.out.println("Bad response");
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+
+                System.out.println("Fail on getPoints operation");
+                /*
+                Error callback
+                */
+            }
+        });
     }
 }
